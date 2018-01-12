897b1a18cb6b523b0c8014f63f7c0c302391f494
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-12 07:31:56 -0600

    Add some conditional rendering

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,42 @@
-console.log('app.js is running');
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One','Two'],
+};
 
-let template = <h1>Indecision App</h1>;
+let template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    {app.options.length > 0 ? 'Here are your options' : 'No options'}
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+  </ol>
+  </div>
+);
+
+const user = {
+  name: 'Mehmet',
+  age: 32,
+  location: 'Austin',
+};
+
+function getLocation(location) {
+  if(location) {
+    return <p>Location: {location}</p>;
+  } 
+}
+
+let templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
+  </div>
+);
+
+
 const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
