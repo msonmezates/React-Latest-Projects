001c4641e10d2c0a02219d686f3a32a921a6b4bf
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-18 07:58:55 -0600

    Render items by using props

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -20,12 +20,18 @@ var IndecisionApp = function (_React$Component) {
   _createClass(IndecisionApp, [{
     key: 'render',
     value: function render() {
+      var title = 'Indecision';
+      var subtitle = 'Put your life in the hands of a computer.';
+      var options = ['Thing one', 'Thing two', 'Thing four'];
       return React.createElement(
         'div',
         null,
-        React.createElement(Header, null),
+        React.createElement(Header, {
+          title: title,
+          subtitle: subtitle
+        }),
         React.createElement(Action, null),
-        React.createElement(Options, null),
+        React.createElement(Options, { options: options }),
         React.createElement(AddOption, null)
       );
     }
@@ -52,12 +58,12 @@ var Header = function (_React$Component2) {
         React.createElement(
           'h1',
           null,
-          'Indecison'
+          this.props.title
         ),
         React.createElement(
           'h2',
           null,
-          'Put your life in the hands of a computer.'
+          this.props.subtitle
         )
       );
     }
@@ -108,8 +114,9 @@ var Options = function (_React$Component4) {
       return React.createElement(
         'div',
         null,
-        'Options Component',
-        React.createElement(Option, null)
+        this.props.options.map(function (option) {
+          return React.createElement(Option, { key: option, optionText: option });
+        })
       );
     }
   }]);
@@ -132,7 +139,7 @@ var Option = function (_React$Component5) {
       return React.createElement(
         'div',
         null,
-        'Option Nested Component'
+        this.props.optionText
       );
     }
   }]);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,11 +1,17 @@
 
 class IndecisionApp extends React.Component {
   render () {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hands of a computer.';
+    const options = ['Thing one', 'Thing two', 'Thing four'];
     return (
       <div>
-        <Header />
+        <Header
+          title={title}
+          subtitle={subtitle}
+        />
         <Action />
-        <Options />
+        <Options options={options} />
         <AddOption />
       </div>
     );
@@ -16,8 +22,8 @@ class Header extends React.Component {
   render () {
     return (
       <div>
-        <h1>Indecison</h1>
-        <h2>Put your life in the hands of a computer.</h2>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
       </div>
     );
   }
@@ -37,8 +43,9 @@ class Options extends React.Component {
   render () {
     return (
       <div>
-        Options Component
-        <Option />
+        {
+          this.props.options.map(option => <Option key={option} optionText={option} />)
+        }
       </div>
     );
   }
@@ -48,7 +55,7 @@ class Option extends React.Component {
   render () {
     return (
       <div>
-        Option Nested Component
+        {this.props.optionText}
       </div>
     );
   }
