a8aeb437e6f784f1014707f6515a9d79d521932c
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-17 21:41:39 -0600

    Enhance the example with subclasses

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -4,17 +4,46 @@ class Person {
     this.age = age;
   }
   getGreeting() {
-    return `Hi, ${this.name}!`;
+    return `Hi, I am ${this.name}!`;
   }
   getDescription() {
     return `${this.name} is ${this.age} year(s) old.`
   }
 }
 
-const me = new Person('Andrew Mead', 26);
-console.log(me.getGreeting());
-console.log(me.getDescription());
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
+    if(this.hasMajor()) {
+      description += ` Their major is ${this.major}.`;
+    }
+    return description;
+  }
+}
 
-const other = new Person();
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+  getGreeting() {
+    let greeting = super.getGreeting();
+    if(this.homeLocation) {
+      greeting += ` I'm visiting from ${this.homeLocation}.`;
+    }
+    return greeting;
+  }
+}
+
+const me = new Traveler('Andrew Mead', 26, 'Philadelphia');
+console.log(me.getGreeting());
+
+const other = new Traveler();
 console.log(other.getGreeting());
-console.log(other.getDescription());
