596b1aab5cf65af9df832140a5189d00e399fc1e
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-18 20:06:04 -0600

    Add a form, submit and remove button

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -30,19 +30,28 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert('handlePick');
+  }
+
   render () {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert('handleRemoveAll')
+  }
+
   render () {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
         {
           this.props.options.map(option => <Option key={option} optionText={option} />)
         }
@@ -62,10 +71,21 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim(); //this prevents to submit empty string
+    if(option) {
+      alert(option);
+    }
+  }
+
   render () {
     return (
       <div>
-        AddOption Component
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
       </div>
     );
   }
