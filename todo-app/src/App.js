b36ad341cdabc3001314d36cd6ecf731ffbe64a6
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-02-02 22:28:46 -0600

    Add component to display todo items

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Finish work',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Meeting with clients',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Cook dinner',
+        completed: false
+      },
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>App</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
---
+++ todo-app/src/components/TodoItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          {this.props.todo.title}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
