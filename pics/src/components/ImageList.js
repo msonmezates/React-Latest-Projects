38069a2e08799bbfd98282ad6919a5669a5b8097
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-02-08 09:31:24 -0600

    Render searched items with css grid

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ImageCard from './ImageCard';
+
+import './ImageList.css';
+
 const ImageList = (props) => {
-  const images = props.images.map(({ urls, id, description }) => {
-    return <img src={urls.regular} alt={description} key={id} />
+  const images = props.images.map((image) => {
+    return <ImageCard image={image} key={image.id} />
   });
   return (
-    <div>
+    <div className="image-list">
       {images}
     </div>
   );
