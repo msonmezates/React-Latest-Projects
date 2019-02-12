b1de396d4c44eb914cba26f21f81c9b6de3c5895
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-02-12 14:50:39 -0600

    Make api call with redux-thunk middleware

---
+++ blog/src/actions/index.js
@@ -0,0 +1,7 @@
+import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+
+export const fetchPost = () => async dispatch => {
+  const response = await jsonPlaceHolder.get('/posts');
+
+  dispatch({ type: 'FETCH_POST', payload: response });
+};
