cf61c945dae60d35fd240417db7d03d2655dc402
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-02-19 23:04:18 -0600

    Switch to a class component and render list of streams

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
 
-const StreamList = (props) => {
-  return (
-    <div>
-      StreamList
-    </div>
-  );
+class StreamList extends Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  renderList = () => {
+    return this.props.streams.map(stream => {
+      return (
+        <div key={stream.id} className="item">
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">
+              {stream.description}
+            </div>
+          </div>
+        </div>
+      );
+    })
+  };
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">
+          {this.renderList()}
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ streams }) => {
+  return {
+    streams: Object.values(streams) // Switch streams object into an array
+  };
 };
 
-export default connect(null, { fetchStreams })(StreamList);
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
