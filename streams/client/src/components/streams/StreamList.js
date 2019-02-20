09d1601d149ca2e05a963c3ed8d9a78450568b78
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-02-20 10:45:56 -0600

    Show create, edit and delete buttons when user is logged in

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends Component {
+
   componentDidMount() {
     this.props.fetchStreams();
   }
+
+  renderAdmin = (stream) => {
+    if (this.props.currentUserId === stream.userId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  };
+
+  renderCreate = () => {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  };
+
   renderList = () => {
     return this.props.streams.map(stream => {
       return (
         <div key={stream.id} className="item">
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -19,8 +46,9 @@ class StreamList extends Component {
           </div>
         </div>
       );
-    })
+    });
   };
+
   render() {
     return (
       <div>
@@ -28,14 +56,17 @@ class StreamList extends Component {
         <div className="ui celled list">
           {this.renderList()}
         </div>
+        {this.renderCreate()}
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ streams }) => {
+const mapStateToProps = ({ streams, auth }) => {
   return {
-    streams: Object.values(streams) // Switch streams object into an array
+    streams: Object.values(streams), // Switch streams object into an array
+    currentUserId: auth.userId,
+    isSignedIn: auth.isSignedIn
   };
 };
 
