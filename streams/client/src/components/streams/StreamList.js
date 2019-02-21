90c663ca8097d176121c5b9a2619cf5fc869e279
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-02-20 21:26:51 -0600

    Create a reusable form component and refactor stream create page

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -13,9 +13,9 @@ class StreamList extends Component {
     if (this.props.currentUserId === stream.userId) {
       return (
         <div className="right floated content">
-          <button className="ui button primary">Edit</button>
-          <button className="ui button negative">Delete</button>
-        </div>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
+          <Link to={`/streams/delete/${stream.id}`} className="ui button negative">Delete</Link>
+        </div >
       );
     }
   };
