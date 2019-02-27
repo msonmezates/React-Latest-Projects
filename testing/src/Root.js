e94a0c18ce8240b252f5fabb7d52e4212c67080e
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-02-27 06:14:44 -0600

    Implement a new button and its functionality to display list of comments

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import reduxPromise from 'redux-promise';
 
 import reducers from 'reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default ({ children, initialState = {} }) => {
-	return (
-		<Provider store={createStore(reducers, initialState, composeEnhancers(applyMiddleware()))}>{children}</Provider>
-	);
+	const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(reduxPromise)));
+
+	return <Provider store={store}>{children}</Provider>;
 };
