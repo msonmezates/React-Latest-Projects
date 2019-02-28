0309d2b0900d644a7c61333eea3a5efd43925b4f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-02-28 15:42:10 -0600

    Add a state schema and implement a middleware to check schema

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -4,11 +4,12 @@ import { createStore, applyMiddleware, compose } from 'redux';
 
 import reducers from 'reducers';
 import async from 'middlewares/async';
+import stateValidator from 'middlewares/stateValidator';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default ({ children, initialState = {} }) => {
-	const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(async)));
+	const store = createStore(reducers, initialState, composeEnhancers(applyMiddleware(async, stateValidator)));
 
 	return <Provider store={store}>{children}</Provider>;
 };
