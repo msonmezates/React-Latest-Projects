c5fddfc0ec46db7949d3e28a018d757c9503e71a
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-03-04 09:10:00 -0600

    Create a route, user model and connect to db

--- auth/server/index.js
+++ auth/server/index.js
@@ -2,12 +2,21 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const router = require('./router');
+const User = require('./models/user');
 
 const app = express();
+
+// Mongodb Setup
+mongoose.connect('mongodb://localhost:auth/auth', { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
 
 // Middleware Setup
 app.use(morgan('combined')); // http request logger
 app.use(bodyParser.json({ type: '*/*' })); // wildcard is for any request that will be parsed
+router(app);
 
 // Server Setup
 const PORT = process.env.PORT || 5000;
