d638f248b065b923c83e8458e558cc7ab01a9e97
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2019-04-20 21:47:58 -0500

    Create cart subcomponents for cart page

--- shopping-store/src/context.js
+++ shopping-store/src/context.js
@@ -9,9 +9,12 @@ class ProductProvider extends Component {
 	state = {
 		products: [],
 		detailProduct,
-		cart: [],
+		cart: storeProducts,
 		modalOpen: false,
-		modalProduct: detailProduct
+		modalProduct: detailProduct,
+		cartSubTotal: 10,
+		cartTax: 20,
+		cartTotal: 30
 	};
 
 	componentDidMount() {
@@ -85,6 +88,22 @@ class ProductProvider extends Component {
 		});
 	};
 
+	increment = (id) => {
+		console.log('increment method');
+	};
+
+	decrement = (id) => {
+		console.log('decrement method');
+	};
+
+	removeItem = (id) => {
+		console.log('remove item method');
+	};
+
+	clearCart = () => {
+		console.log('clear cart method');
+	};
+
 	render() {
 		return (
 			<ProductContext.Provider
@@ -93,7 +112,11 @@ class ProductProvider extends Component {
 					handleDetail: this.handleDetail,
 					addToCart: this.addToCart,
 					openModal: this.openModal,
-					closeModal: this.closeModal
+					closeModal: this.closeModal,
+					increment: this.increment,
+					decrement: this.decrement,
+					removeItem: this.removeItem,
+					clearCart: this.clearCart
 				}}
 			>
 				{this.props.children}
